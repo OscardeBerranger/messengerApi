dd0fc64fa065aa5b6c6a02865f6ef110a67a341d
o2ber <Oscar.De-Berranger@etu.univ-grenoble-alpes.fr>
2022-11-16 15:07:17 +0100

    fini

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const baseURL = "https://139.162.156.85:8000/"
+const baseURL = "https://192.168.12.246:8000"
 
 
 const mainContainer = document.querySelector("#main")
 const messagesPageButton = document.querySelector("#messagesPage")
 const autrechoseButton = document.querySelector("#autrechose")
+const signInPageButton = document.querySelector('#signInPage')
+let currentUserI = 39
 
 
 messagesPageButton.addEventListener("click", displayMessagesPage)
-autrechoseButton.addEventListener("click", displayAutreChose)
-
+autrechoseButton.addEventListener("click", displayRegisterPage)
+signInPageButton.addEventListener('click', displaySignInPage)
 
 function clearMainContainer(){
     mainContainer.innerHTML= ""
 }
-
 
 function display(content){
     //vider la div principale
@@ -39,8 +40,17 @@ function getMessageTemplate(message){
 function getMessagesTemplate(messages){
 
     let messagesTemplate = ""
+    let template = `
+        <label for="userMessage"></label>
+        <input type="text" name="" id="userMessage" placeholder="your message">
+        <button class="send">Send</button>
+        <button class="refresh">refresh</button>
+        `
+    messagesTemplate+= template
 
     messages.forEach(message=>{
+
+
 
       messagesTemplate+=  getMessageTemplate(message)
     })
@@ -50,13 +60,22 @@ function getMessagesTemplate(messages){
 }
 
 function getRegisterTemplate(){
-    let template = ``
+    let template =`
+                <div class="loginForm">
+                <h1>Sign Up</h1>
+                <label for="logUsername">Username</label>
+                <input type="text" name="username" id="regUsername" placeholder="Enter your new username">
+                <label for="logPassword">Password</label>
+                <input type="password" name="password" id="regPassword" placeholder="Enter your new password">
+                <button id="register">Sign up</button>
+</div>
+    `
     return template
 }
 
 async function getMessagesFromApi(){
 
-    let url = `${baseURL}messages/`
+    let url = `${baseURL}/api/messages/`
 
   return await fetch(url)
         .then(response=>response.json())
@@ -74,16 +93,91 @@ function displayMessagesPage(){
         display(
             getMessagesTemplate(messages)
             )
+        document.querySelector('.send').addEventListener('click', ()=>{
+            sendMessage(document.querySelector('#userMessage').value)
+        })
+        document.querySelector('.refresh').addEventListener('click', ()=>{
+            clearMainContainer()
+            displayMessagesPage()
+        })
 
 
     })
 
 }
 
-function displayAutreChose(){
-    display("voila autrechose")
+function fregister(username, password) {
+    let url = `${baseURL}/register`
+    let body = {
+        username: username,
+        password: password
+    }
+    let bodySerialise = JSON.stringify(body)
+
+    let fetchParams = {
+        method : "POST",
+        body: bodySerialise
+
+    }
+
+
+    fetch(url, fetchParams)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
+            currentUserI = data.id
+        })
+}
+
+function loadForRegister(){
+    let username = document.querySelector('#regUsername').value
+    let password = document.querySelector('#regPassword').value
+    console.log(username, password)
+    fregister(username, password)
 }
 
 function displayRegisterPage(){
-    display(getRegisterTemplate)
+    let registerPage = getRegisterTemplate()
+    display(registerPage)
+    let bouton = document.querySelector('#register')
+    bouton.addEventListener('click', ()=>{
+        loadForRegister()
+    })
+}
+
+function sendMessage(userMessage){
+
+    let url = `${baseURL}messages/${currentUserI}/new`
+
+    let body = {
+        content : userMessage
+    }
+    let bodySerialise = JSON.stringify(body)
+    let fetchParam = {
+        method : "POST",
+        body:bodySerialise
+    }
+    fetch(url, fetchParam)
+        .then(response=>response.json())
+        .then(data=>console.log(data))
+
+    clearMainContainer()
+    displayMessagesPage()
+}
+
+function getSignInTemplate(){
+    let template = `
+            <h2>Sign In</h2>
+            <label for="signInUserName"></label>
+            <input type="text" name="signInUserName" id="signInUserName" placeholder="Username">
+            <label for="signInPassword"></label>
+            <input type="password" name="signInPassword" id="signInPassword" placeholder="password">
+
+    `
+    return template
+}
+
+function displaySignInPage(){
+    let signInPage = getSignInTemplate()
+    display(signInPage)
 }
