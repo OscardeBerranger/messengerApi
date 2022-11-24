b9ad9a2e8af3172da4ab39ce672217fbd1e5de12
o2ber <Oscar.De-Berranger@etu.univ-grenoble-alpes.fr>
2022-11-24 11:22:20 +0100

    Version avec suppression des messages fonctionelle

--- main.js
+++ main.js
@@ -1,36 +1,35 @@
-const baseURL = "https://172.104.149.64"
-let token ;
+const baseURL = "https://b1messenger.tk"
+let token;
 let currenUserId;
 
-const mainContainer = document.querySelector("#main")
-const messagesPageButton = document.querySelector("#messagesPage")
-const autrechoseButton = document.querySelector("#autrechose")
-const signInPageButton = document.querySelector('#signInPage')
-const myModal = document.querySelector('#myModal')
+const mainContainer = document.querySelector("#main");
+const messagesPageButton = document.querySelector("#messagesPage");
+const autrechoseButton = document.querySelector("#autrechose");
+const signInPageButton = document.querySelector('#signInPage');
+const myModal = document.querySelector('#myModal');
 const closeModal = document.querySelector(".closeModal");
-const toTheTop = document.querySelector('.toTheTop')
+const toTheTop = document.querySelector('.toTheTop');
+const editMe = document.querySelector('.edit')
 
 
 messagesPageButton.addEventListener("click", ()=>{
     displayMessagesPage()
     document.querySelector('.containerMainTitle').style.opacity = 0
-})
-
+});
 autrechoseButton.addEventListener("click", ()=>{
     displayRegisterPage()
     document.querySelector('.containerMainTitle').style.opacity = 0
-})
-
+});
 signInPageButton.addEventListener('click', ()=>{
     displaySignInPage()
     document.querySelector('.containerMainTitle').style.opacity = 0
-})
+});
 closeModal.addEventListener('click', ()=>{
     myModal.style.display = "none"
-})
+});
 toTheTop.addEventListener('click', ()=>{
     window.scrollTo(0, 0)
-})
+});
 
 //Chargement des messages
 async function getMessagesFromApi(){
@@ -84,7 +83,6 @@ function getMessageTemplate(message){
         time = diffMin + " minutes"
     }else time = diffSec + " seconds"
 
-    console.log(message)
     let template
 
     if (message.author.username == currenUserId){
@@ -92,13 +90,14 @@ function getMessageTemplate(message){
                             <div class="row border border-dark">
                                 <p>Author : ${message.author.username}</p>
                                 <p> Sent  ${time} ago.</p>
-                                <p>${message.content}<button class="deleteMsg" id="${message.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg></button></p>
+                                <p>${message.content}<button class="btnMsg deleteMsg" id="${message.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg></button> <button class="btnMsg editMsg" id="${message.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"> <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/> </svg></button></p>
                             </div>
                         `
     }else {template =  `
                             <div class="row border border-dark">
                                 <p>Author : ${message.author.username}</p>
                                 <p> Sent  ${time} ago.</p>
+                                <button class="btnReply" id="${message.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply-all" viewBox="0 0 16 16"> <path d="M8.098 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L8.8 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L4.114 8.254a.502.502 0 0 0-.042-.028.147.147 0 0 1 0-.252.497.497 0 0 0 .042-.028l3.984-2.933zM9.3 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/> <path d="M5.232 4.293a.5.5 0 0 0-.7-.106L.54 7.127a1.147 1.147 0 0 0 0 1.946l3.994 2.94a.5.5 0 1 0 .593-.805L1.114 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.5.5 0 0 0 .042-.028l4.012-2.954a.5.5 0 0 0 .106-.699z"/> </svg></button>
                             </div>
                         `}
     return template
@@ -120,6 +119,9 @@ function getMessagesTemplate(messages){
     messages.forEach(message=>{
         messagesTemplate+=getMessageTemplate(message)
     })
+    let templateEdit = `
+    
+    `
     return messagesTemplate
 
 }
@@ -138,6 +140,27 @@ function getRegisterTemplate(){
     return template
 }
 
+//Creation de la template du formulaire edit/reply
+function getEditorTemplate(type){
+    let template
+    if (type === "edit") {
+        template =`
+            <div class="editor">
+                <input type="text" name="textEdit" id="textEdit" placeholder="edit">
+                <button id="editMsgButton">Send</button>
+            </div>
+        `
+    }else if (type === "reply"){
+        template =`
+            <div class="editor">
+                <input type="text" name="textReply" id="textReply" placeholder="reply">
+                <button id="replyMsgButton">Send</button>
+            </div>
+        `
+    }
+    return template
+}
+
 //Supprime tout les messages de la page
 function clearMainContainer(){
     mainContainer.innerHTML= ""
@@ -147,7 +170,6 @@ function clearMainContainer(){
 function loadForRegister(){
     const username = document.querySelector('#regUsername')
     const password = document.querySelector('#regPassword')
-    console.log(password)
     if (password.value !== ""){
         fregister(username.value, password.value)
     }else {
@@ -183,6 +205,15 @@ function displayMessagesPage(){
             displayMessagesPage()
         })
 
+        const editBtns = document.querySelectorAll('.editMsg')
+        editBtns.forEach(btn=>{
+            btn.addEventListener('click',()=>{
+                displayEditOrReplyArea("edit")
+                let currentMsg = btn.id
+                editMsg(currentMsg)
+            })
+        })
+
         const delBtns = document.querySelectorAll('.deleteMsg')
         delBtns.forEach(btn=>{
             btn.addEventListener('click',()=>{
@@ -194,11 +225,17 @@ function displayMessagesPage(){
                             'Content-Type':'application/json'},
                     }
                     fetch(url, fetchParams)
-                        .then((response)=>response.json())
-                        .then(response=>{
-                            displayMessagesPage()
-                        })
-            } )
+                    displayMessagesPage()
+            })
+        })
+
+        const replyBtn = document.querySelectorAll('.btnReply')
+        replyBtn.forEach(btn=>{
+            btn.addEventListener('click', ()=>{
+                displayEditOrReplyArea("reply")
+                let currentMsg = btn.id
+                replyMsg(currentMsg)
+            })
         })
 
         window.scrollTo(0, document.body.scrollHeight);
@@ -235,6 +272,13 @@ function displaySignInPage(){
     if (!mainContainer.classList.contains("active")){
         mainContainer.classList.toggle('active')
     }
+}
+
+//Affiche le text area permettant d'éditer/reply à un message
+function displayEditOrReplyArea(type){
+    editMe.innerHTML = ""
+    let template = getEditorTemplate(type)
+    editMe.innerHTML = template
 }
 
 //Permet d'envoyer un message
@@ -331,10 +375,56 @@ function fregister(username, password){
     fetch(url, fetchParams)
         .then(response=>response.json())
         .then(data=> {
-                console.log(data)
                 if (typeof data == "string"){
                     myModal.style.display = "block";
                 }
             })
 
+}
+
+//Permet d'éditer un message
+function editMsg(message){
+    let currentEditBtn = document.querySelector('#editMsgButton')
+    currentEditBtn.addEventListener('click', ()=>{
+        let url = `${baseURL}/api/messages/${message}/edit`
+        let body = {
+            content:document.querySelector('#textEdit').value
+        }
+        let bodySerialise = JSON.stringify(body)
+        let fetchParams = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+
+            },
+            body: bodySerialise
+        }
+        fetch(url, fetchParams)
+        displayMessagesPage()
+        editMe.innerHTML = ""
+    })
+}
+
+//Permet de répondre à un message
+function replyMsg(message){
+    let currentReplyBtn= document.querySelector('#replyMsgButton')
+    currentReplyBtn.addEventListener('click', ()=>{
+        let url = `${baseURL}/api/responses/${message}/new`
+        let body = {
+            content:document.querySelector('#textReply').value
+        }
+        let bodySerialise = JSON.stringify(body);
+        let fetchParams = {
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body:bodySerialise
+        }
+        fetch(url, fetchParams)
+        displayMessagesPage()
+        editMe.innerHTML = ""
+    })
 }
